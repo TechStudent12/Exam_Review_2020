a05a7ae4601959c76f9c3d842775f6bca9d29bec
SwordLord22 <60618536+SwordLord22@users.noreply.github.com>
2020-04-19 00:46:44 -0400

    Update LinkedListPratice.c

--- LinkedListPratice.c
+++ LinkedListPratice.c
@@ -1,4 +1,9 @@
-/*#include <stdio.h> 
+/*
+ * Author: Sword Lord of The Lonely Peak
+ * Date: 2020/05/18
+ */
+
+ #include <stdio.h> 
  #include <stdlib.h> 
  
  typedef struct myList {
@@ -10,13 +15,13 @@
  
  int main()
  {
-     Node *a = _getnode(); 
-     Node *b = _getnode(); 
-     Node *c = _getnode(); 
+     Node *a = _getnode(); // First node
+     Node *b = _getnode(); // Second node
+     Node *c = _getnode(); // Third node
      
      a -> link = NULL; 
      b -> link = NULL; 
-     c -> link = NULL; 
+     c -> link = NULL;
      
      printf("a data = ?");
      scanf("%d", &a->info);
@@ -40,7 +45,9 @@
  Node *_getnode() 
  {
      return((Node *)malloc(sizeof(Node)));
- }*/
+ }
+
+/*******************************************************************************************************************************/
  
  #include <stdio.h> 
  #include <stdlib.h> 
@@ -54,41 +61,63 @@
  
  int main()
  {
-     /*
      Node *a = _getnode(); 
      Node *t = a; 
-     a -> link = NULL; 
-     a -> link = _getnode(); 
-     a -> link -> link = _getnode(); 
-     a -> link -> link -> link= NULL; 
+  
+     a -> link = NULL; //First Node
+     a -> link = _getnode(); //Second Node
+     a -> link -> link = _getnode(); // Third node
+     a -> link -> link -> link= NULL; // Fourth node 
      
      printf("a data = ?");
-     scanf("%d", &a ->info);
+     scanf("%d", &a -> info);
      printf("b data = ?");
-     scanf("%d", &a -> link ->info);
+     scanf("%d", &a -> link -> info);
      printf("c data = ?");
-     scanf("%d", &a -> link -> link ->info);
+     scanf("%d", &a -> link -> link -> info);
      
+     //Prints the values of the linked list!
      while(a != NULL)
      {
          printf("%d -> ", a -> info); 
          a = a -> link; 
      }
      printf("NULL\n"); 
-     a = t; 
+     a = t; //Make sure to this before reprinting the linked list to make that a points at the first node and not the last node. 
      while(a != NULL)
      {
          printf("%d -> ", a -> info); 
          a = a -> link; 
      }
      printf("NULL\n"); 
-     return 0; */
+     return 0;
+ }
+ 
+ Node *_getnode() 
+ {
+     return((Node *)malloc(sizeof(Node)));
+ }
+
+/*******************************************************************************************************************************/
+
+ #include <stdio.h> 
+ #include <stdlib.h> 
+ 
+ typedef struct myList {
+     int info; 
+     struct myList *link;
+ }Node;
+ 
+ Node *_getnode(); 
+ 
+ int main()
+ {
      Node *s = _getnode();
      s -> link = NULL;
      Node *t = s; 
      char c;
      int true = 1; 
-     while(true)
+     while(true) //Creates x many nodes (user decides the amount). 
      {
          printf("Please enter the data for s info : ");
          scanf("%d", &s -> info); 
@@ -99,11 +128,12 @@
              s -> link = NULL; 
              break; 
          }
-         s -> link = _getnode(); 
-         s = s -> link; 
+         s -> link = _getnode(); // create next node. 
+         s = s -> link; // make s equal to the next node. 
       }
       s = t; 
-      while(s != NULL)
+      //print the values of the linked list. 
+      while(s != NULL) 
       {
           printf("%d -> ", s->info);
           s = s->link; 
@@ -113,5 +143,7 @@
  
  Node *_getnode() 
  {
-     return((Node *)malloc(sizeof(Node)));
+     return((Node *)malloc(sizeof(Node))); //allocate memory for the node. 
  }
+
+/*******************************************************************************************************************************/
